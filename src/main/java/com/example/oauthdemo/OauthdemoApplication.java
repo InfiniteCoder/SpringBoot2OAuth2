ff15255769538bb94104be2d040e169db7ab8658
Aditya Patil <contactadityapatil@yahoo.in>
2017-12-24 20:40:27 +0530

    Simplify things

--- src/main/java/com/example/oauthdemo/OauthdemoApplication.java
+++ src/main/java/com/example/oauthdemo/OauthdemoApplication.java
@@ -1,10 +1,7 @@
 package com.example.oauthdemo;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.oauth2.client.registration.ClientRegistration;
-import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,20 +10,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class OauthdemoApplication {
 
-	private ClientRegistrationRepository repository;
-
-    @Autowired
-    public OauthdemoApplication(ClientRegistrationRepository repository) {
-        this.repository = repository;
-    }
-
     public static void main(String[] args) {
 		SpringApplication.run(OauthdemoApplication.class, args);
 	}
 
 	@RequestMapping("/")
 	@ResponseBody
-	public ClientRegistration example(){
-		return repository.findByRegistrationId("google");
+    public String example() {
+        return "Say something nice to user";
 	}
 }
